0c3a372ed796c0f7a46829cc52b9be6d24c6d708
daiyanabdul <daiyanabdul1@gmail.com>
2025-01-30 00:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+function insert_Row() {  
+
+	const table = document.getElementById("sampleTable");
+
+    // Create a new row
+    const newRow = document.createElement("tr");
+
+    // Create new cells
+    const cell1 = document.createElement("td");
+    cell1.textContent = "New Cell1";
+    const cell2 = document.createElement("td");
+    cell2.textContent = "New Cell2";
+
+    // Append cells to the new row
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+
+    // Insert the new row at the top (index 0)
+     table.insertBefore(newRow, table.firstChild);
 }
+
